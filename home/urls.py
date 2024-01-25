69e557b09908d6aadcd2a7648647c470a85fa026
PrateekLC <lamichhaneprateek@gmail.com>
2024-01-25 16:58:35 +0545

    linkingnavbar

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,12 @@ from .views import *
 from django.urls import path, include
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('about',about,name='about'),
+    path('services', services, name='services'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('pricing', pricing,name='pricing'),
+    path('blog', blog,name='blog'),
+    path('contact',contact, name='contact')
+
 ]
